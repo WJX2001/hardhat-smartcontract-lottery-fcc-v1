2acd1fabe27468c90699ef999233365465fc9070
WJX2001 <1758807220@qq.com>
2024-11-04 22:54:28 +0800

    feat: erc20 token

--- test/uint/Raffle.test.js
+++ test/uint/Raffle.test.js
@@ -2,24 +2,26 @@
 const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 const { assert, expect } = require('chai')
+const { toNumber } = require("ethers")
 !developmentChains.includes(network.name) ? describe.skip : describe("Raffle Unit Tests", async function () {
-  let raffle, VRFCoordinatorV2Mock
+  let raffle, VRFCoordinatorV2Mock, raffleEntranceFee, deployer, inteval
   const chainId = network.config.chainId
 
   beforeEach(async function () {
-    const { deployer } = await getNamedAccounts()
+    deployer = (await getNamedAccounts()).deployer
     await deployments.fixture(["all"])
     raffle = await ethers.getContract("Raffle", deployer)
     VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+    raffleEntranceFee = await raffle.getEntranceFee()
+    inteval = await raffle.getInterval()
   })
 
   describe("constructor", async function () {
     it("initializes the raffle correctly", async function () {
       // Ideally we make our tests have just 1 assert per "it"
       const raffleState = await raffle.getRaffleState()
-      const interval = await raffle.getInterval()
       assert.equal(raffleState.toString(), "0")
-      assert.equal(interval.toString(), networkConfig[chainId]["interval"])
+      assert.equal(inteval.toString(), networkConfig[chainId]["interval"])
     })
   })
 
@@ -29,6 +31,29 @@ const { assert, expect } = require('chai')
       await expect(raffle.enterRaffle())
         .to.be.rejectedWith("Raffle__NotEnoughETHEntered")
     })
+
+    it("records players when they enter", async function () {
+      await raffle.enterRaffle({ value: raffleEntranceFee })
+      const playerFromContract = await raffle.getPlayer(0)
+      assert.equal(playerFromContract, deployer)
+    })
+
+    it("emits event on enter", async function () {
+      await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.emit(raffle, "RaffleEnter")
+    })
+
+    it("doesnt allow entrance when raffle is calculating", async function () {
+      const tmp = ethers.toNumber(inteval)
+      await raffle.enterRaffle({ value: raffleEntranceFee })
+      // 为区块增加了时间，并向前挖了一个区块
+      // https://hardhat.org/hardhat-network/docs/reference#hardhat_setstorageat
+      await network.provider.send("evm_increaseTime", [ethers.toNumber(inteval)+1])
+      await network.provider.send("evm_mine", [])
+      await raffle.performUpkeep([]) // 这时进入了calculating状态
+      await expect(raffle.enterRaffle({value: raffleEntranceFee})).to.be.revertedWith("Raffle_NotOpen")
+    })
   })
+
+
 
 })
